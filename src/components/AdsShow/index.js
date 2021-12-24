01f9e178bfd668976290025dd7f88362b5714502
Алексей Денисов <C:\Program Files (x86)\The Bat!\Mail>
2021-12-24 18:35:31 +0300

    AdsShow output

--- src/components/AdsShow/index.js
+++ src/components/AdsShow/index.js
@@ -1,39 +1,51 @@
+import { useSelector } from "react-redux";
 import "./AdsShow.css";
 
 export default function AdsShow() {
+
+  const data = useSelector((state) => state.adForm);
+
+  const checkURL = (url) => {
+    const domain = url.match(/^(?:https?:\/\/)?(?:www\.)?([^:/?=]+)/i);
+    console.log(domain);
+    return domain ? domain[1] : 'yandex.ru';
+  }
+
   return (
     <div className="AdsShow">
     <h2 className="AdsShow__title">
       <a className="AdsShow__title-link" href="#1">
-        <div className="AdsShow__favicon" style={{backgroundImage: 'url(https://favicon.yandex.net/favicon/cdm-moscow.ru?size=32)'}}/>
-        <div className="AdsShow__title-text">ЦДМ на Лубянке — Приходите к нам!</div>
+        <div className="AdsShow__favicon" style={{backgroundImage: 'url(https://favicon.yandex.net/favicon/' + checkURL(data.url) + '?size=32)'}}/>
+        <div className="AdsShow__title-text">{data.header || 'Заголовок объявления'} {data.extraheader}</div>
       </a>
       </h2>
       <div className="AdsShow__path">
-        <a className="AdsShow__path_link" href="#1"><b>life-clinic.ru</b><span className="AdsShow__path_separator">&rsaquo;</span>медицинская-клиника</a>
+        <a className="AdsShow__path_link" href="#1"><b>{data.url || 'домен'}</b><span className="AdsShow__path_separator">&rsaquo;</span>{data.showurl || data.header || 'Заголовок объявления'}</a>
         <div className="AdsShow__path_dots" />
       </div>
       <div className="AdsShow__content">
         <span className="AdsShow__content-adv">Реклама<span className="AdsShow__content-dot">&nbsp;· </span></span>
-        Главное детское приключение! Магазины и развлечения на любой вкус. Шоу и конкурсы.
-        <span className="AdsShow__content-dot">&nbsp;· </span>
-        7 этажей. 365 дней в году. 91 магазин. 32 ресторана. 15 зон развлечений
+        {data.text || 'Текст объявления'}
+        {data.callout.length ? 
+          <span className="AdsShow__content-dot">&nbsp;· </span> + ( 
+        data.callout.map((callout) => callout ? callout + '. ' : "")
+        ) : null }
       </div>
       <div className="AdsShow__sitelinks">
         <div className="AdsShow__sitelinks-item">
-          <a className="AdsShow__sitelinks-title" href="#1">Кинотеатр</a>
+          <a className="AdsShow__sitelinks-title" href="#1">Ссылка 1</a>
           <div className="AdsShow__sitelinks-description">Самые современные блокбастеры и детские сеансы в 8 залах</div>
         </div>
         <div className="AdsShow__sitelinks-item">
-          <a className="AdsShow__sitelinks-title" href="#1">Кинотеатр</a>
+          <a className="AdsShow__sitelinks-title" href="#1">Ссылка 2</a>
           <div className="AdsShow__sitelinks-description">Самые современные блокбастеры и детские сеансы в 8 залах</div>
         </div>
         <div className="AdsShow__sitelinks-item">
-          <a className="AdsShow__sitelinks-title" href="#1">Кинотеатр</a>
+          <a className="AdsShow__sitelinks-title" href="#1">Ссылка 3</a>
           <div className="AdsShow__sitelinks-description">Самые современные блокбастеры и детские сеансы в 8 залах</div>
         </div>
         <div className="AdsShow__sitelinks-item">
-          <a className="AdsShow__sitelinks-title" href="#1">Кинотеатр</a>
+          <a className="AdsShow__sitelinks-title" href="#1">Ссылка 4</a>
           <div className="AdsShow__sitelinks-description">Самые современные блокбастеры и детские сеансы в 8 залах</div>
         </div>
       </div>
